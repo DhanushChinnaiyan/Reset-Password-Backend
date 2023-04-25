55c6211b559b25408e67c42c4bd6a8c980e260a1
DhanushChinnaiyan <dhanushmd4021@gmail.com>
2023-04-25 13:09:10 +0530

    f

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ const dataBaseConnection =() => {
         useUnifiedTopology:true
     }
     try {
-        mongoose.connect(process.env.MONGODB_URI,params)
+        mongoose.connect(process.env.MONGO_URL,params)
     console.log("mongoDB connected")
         
     } catch (error) {
