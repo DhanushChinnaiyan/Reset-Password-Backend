b5b8cedecd3c4d3d70f4682b15c3dfef335df455
DhanushChinnaiyan <dhanushmd4021@gmail.com>
2023-04-25 12:34:42 +0530

    vercel error checking

--- db.js
+++ db.js
@@ -1,10 +1,4 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-
-// dot env configuration;
-dotenv.config()
-
-
 
 const dataBaseConnection =() => {
     const params = {
@@ -12,7 +6,7 @@ const dataBaseConnection =() => {
         useUnifiedTopology:true
     }
     try {
-        mongoose.connect("mongodb+srv://dhanush:621417114021@cluster0.yv1vvqj.mongodb.net/PASSWORDRESET?retryWrites=true&w=majority",params)
+        mongoose.connect(process.env.MONGODB_URL,params)
     console.log("mongoDB connected")
         
     } catch (error) {
